6879b2d064e732860ff65c0590af78f5bbf29241
Alexey Goncharov <x401om@gmail.com>
2024-05-09 18:59:35 +0100

    update AdaptySDK dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["AdaptyUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adaptyteam/AdaptySDK-iOS.git", branch: "dev")
+        .package(url: "https://github.com/adaptyteam/AdaptySDK-iOS.git", "2.11.0" ..< "2.12.0")
     ],
     targets: [
         .target(
